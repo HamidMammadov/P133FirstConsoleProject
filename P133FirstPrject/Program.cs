c76fb64378d8284edb32c15b7ba18847fd0e4077
Hamid.mammadov <Hamid.mammadov@CASATR0>
2022-08-31 11:46:33 +0400

    Add project files.

---
+++ P133FirstPrject/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace P133FirstPrject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a = 6;
+
+            //declare dynamic variable
+            //dynamic b = 5;
+
+            //b = "Test";
+
+
+            //byte age = 32;
+            //sbyte temprature = -45;
+
+            //float pi = 3;
+
+            //decimal test = 5;
+
+            //double point = 65;
+
+            //var ab = point / 2;
+
+            //string numStr = "35";
+
+            //bool check = age == point;
+
+            //if (age == 35)
+            //{
+            //    Console.WriteLine("Qocalmisiniz");
+            //}
+            //else if (age == 33)
+            //{
+            //    Console.WriteLine("Qocalmisiniz");
+            //}
+            //else if (age == 32)
+            //{
+            //    Console.WriteLine("Qocalmisiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("az qalib");
+            //}
+
+            //switch (age)
+            //{
+            //    case 35:
+
+            //        Console.WriteLine(age);
+            //        break;
+            //    case 34:
+            //        Console.WriteLine(age);
+            //        break;
+            //    case 33:
+            //        Console.WriteLine(age);
+            //        break;
+            //    case 32:
+            //        Console.WriteLine(age);
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            //byte i = 10;
+
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //    i++;
+            //}
+
+            //byte j = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("Sert Odemir");
+            //} while (j > 0);
+
+            //while (j > 0)
+            //{
+            //    Console.WriteLine("Sert Odemir while");
+            //}
+
+            //for (int ij = 0; ij < 5; ij++)
+            //{
+            //    Console.WriteLine(ij);
+            //}
+
+            int a = 255;
+        }
+    }
+}
